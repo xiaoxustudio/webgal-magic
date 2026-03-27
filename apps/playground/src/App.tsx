8e58c3d16bbf15893a963b9f1168c166dca58440
xiaoxustudio <xiaoxustudio@foxmail.com>
2026-03-27 23:43:08 +0800

    feat: add parser performance comparison display

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -8,7 +8,9 @@ import textCode from "/start.txt?raw";
 
 interface IDiffObject {
   original: string;
+  originalSpeed: number;
   modified: string;
+  modifiedSpeed: number;
 }
 
 function App() {
@@ -16,17 +18,28 @@ function App() {
   const [diffObject, setDiffObject] = useState<IDiffObject>({
     modified: "",
     original: "",
+    originalSpeed: 0,
+    modifiedSpeed: 0,
   });
   const handleEditorDidMount = (editor: monaco.editor.IDiffEditor) => {
     diffEditorRef.current = editor;
   };
 
   const onParsing = (value?: string) => {
+    const time = {
+      p1: performance.now(),
+      p2: 0,
+    };
     const parsed = parseScript(value ?? "");
+    time.p1 = performance.now() - time.p1;
+    time.p2 = performance.now();
     const parsedForNew = parseScriptForNew(value ?? "");
+    time.p2 = performance.now() - time.p2;
     setDiffObject({
       modified: JSON.stringify(parsed, null, 2),
       original: JSON.stringify(parsedForNew, null, 2),
+      modifiedSpeed: time.p1,
+      originalSpeed: time.p2,
     });
   };
 
@@ -36,8 +49,18 @@ function App() {
         height="50vh"
         defaultLanguage="json"
         defaultValue={textCode}
+        onMount={(editor) => {
+          editor.focus();
+          onParsing(textCode);
+        }}
         onChange={(v) => onParsing(v)}
       />
+      <hr />
+      <p>
+        webgal-parser speed: {diffObject.modifiedSpeed.toFixed(2)}ms
+        <br />
+        webgal-parser-new speed: {diffObject.originalSpeed.toFixed(2)}ms
+      </p>
       <DiffEditor
         height="90vh"
         language="json"
