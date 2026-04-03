0ebdf4df0b9ba87bebae7a9adfe3fe2c477ab367
xiaoxustudio <xiaoxustudio@foxmail.com>
2026-04-03 17:33:29 +0800

    chore: remove vp check --fix from deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite-plus";
 export default defineConfig({
   staged: {
     "*": "vp check --fix",
+    "*.test.ts": "vp test",
   },
   lint: { options: { typeAware: true, typeCheck: true } },
 });
