7f716aa68052ff61c04fb28588f6efe4d0efc43c
xiaoxustudio <xiaoxustudio@foxmail.com>
2026-04-03 17:57:18 +0800

    chore: remove build config from playground

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -5,32 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "webgal-magic",
-  build: {
-    rolldownOptions: {
-      output: {
-        codeSplitting: {
-          groups: [
-            {
-              name: "monaco",
-              test: /node_modules\/monaco-editor/,
-              minSize: 100000, // 100KB
-              maxSize: 250000, // 250KB
-              priority: 15,
-            },
-            {
-              name: "vendor",
-              test: /node_modules/,
-              priority: 10,
-            },
-            {
-              name: "common",
-              minShareCount: 2,
-              minSize: 10000,
-              priority: 5,
-            },
-          ],
-        },
-      },
-    },
-  },
 });
